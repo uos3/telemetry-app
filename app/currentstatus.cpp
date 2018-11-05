94c91db761a2eb9e7fb4c1084d34579bfdf14965
James Brown <james@impromptu-software.co.uk>
2018-11-05 16:02:09 +0000

    Add functionallity to status window refresh button

--- app/currentstatus.cpp
+++ app/currentstatus.cpp
@@ -65,23 +65,21 @@ currentstatus::~currentstatus()
 
 void currentstatus::on_refreshButton_clicked()
 {
-    // qDebug("THE REFRESH BUTTON WAS PRESSED");
-
-    /*
     // Init
     DB read_db("localhost", "cubesat");
     read_db.connect(secrets::username, secrets::password);
 
 
+
     // Setup table view
-    this->status_model = new QSqlTableModel(parent, read_db.get_database());
+    this->status_model = new QSqlTableModel(this->parent(), read_db.get_database());
     this->status_model->setTable("status");
     this->status_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     this->status_model->select();
 
 
     // Setup table view
-    this->gps_model = new QSqlTableModel(parent, read_db.get_database());
+    this->gps_model = new QSqlTableModel(this->parent(), read_db.get_database());
     this->gps_model->setTable("gps");
     this->gps_model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     this->gps_model->select();
@@ -90,6 +88,12 @@ void currentstatus::on_refreshButton_clicked()
 
     status_view->setModel(this->status_model);
     status_view->sortByColumn(0,Qt::DescendingOrder);
+
+    for (int i = 5; i< status_view->model()->rowCount(); i++)
+    {
+        status_view->hideRow(i);
+    }
+
     status_view->show();
 
 
@@ -97,8 +101,13 @@ void currentstatus::on_refreshButton_clicked()
 
     gps_view->setModel(this->gps_model);
     gps_view->sortByColumn(0,Qt::DescendingOrder);
+
+    for (int i = 5; i< gps_view->model()->rowCount(); i++)
+    {
+        gps_view->hideRow(i);
+    }
+
     gps_view->show();
-    */
 }
 
 
