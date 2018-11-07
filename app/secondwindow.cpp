a9b926dd5920e588da1333eca98abe5b91b61d30
James Brown <james@impromptu-software.co.uk>
2018-11-07 14:47:12 +0000

    Add comments to secondwindow.cpp Remove unused variables from secondwindow.cpp

--- app/secondwindow.cpp
+++ app/secondwindow.cpp
@@ -8,31 +8,33 @@
 #include "secrets/secrets.h"
 #include <QDebug>
 
-QString current_query_string = "SELECT * FROM cubesat.status";
-QString column_query_string = "SELECT column_name FROM information_schema.columns WHERE table_name = 'status' and table_schema = 'cubesat'";
-QString result = "";
-QStringList column_names;
-int result_2 = 0;
-int startPosition = 0;
-
 secondwindow::secondwindow(QWidget *parent): QWidget(parent), ui(new Ui::secondwindow)
 {
+
+    // Setup the UI for the status data window
     ui->setupUi(this);
-    // Init
+
+    // Connect to the cubesat database
     DB read_db("localhost", "cubesat");
     read_db.connect(secrets::username, secrets::password);
 
 
-    // Setup table view
+    // Setup the database model
     this->model = new QSqlTableModel(parent, read_db.get_database());
     this->model->setTable("status");
     this->model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     this->model->select();
 
+    // Get the table view
     QTableView *view = ui->status_tableView;
 
+    // Input model into table view
     view->setModel(this->model);
+
+    // Sort table view by first column (Should be pack ID) in descending order
     view->sortByColumn(0,Qt::DescendingOrder);
+
+    // Display the tableview
     view->show();
 
 }
@@ -46,39 +48,57 @@ secondwindow::~secondwindow()
 
 void secondwindow::on_refreshButton_clicked()
 {
-    // qDebug("THE REFRESH BUTTON WAS PRESSED");
+    // Detected refresh button clicked
+    // We should try to reload and display the data
+
+    // Connect to the cubesat database
     DB read_db("localhost", "cubesat");
     read_db.connect(secrets::username, secrets::password);
 
-
-    // Setup table view
+    // Setup the database model
     this->model = new QSqlTableModel(this->parent(), read_db.get_database());
     this->model->setTable("status");
     this->model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     this->model->select();
 
+    // Get the table view
     QTableView *view = ui->status_tableView;
 
+    // Input model into table view
     view->setModel(this->model);
+
+    // Sort the table view by first column (Should be pack ID) in decending order
     view->sortByColumn(0,Qt::DescendingOrder);
+
+    // Display the tableview
     view->show();
 }
 
 
 void secondwindow::on_generateGraphButton_clicked()
 {
-    qDebug("THE GRAPH BUTTON WAS PRESSED");
+    // TODO #enhancement: Graphing functionality is currently unimplemented
+
+    // Get the selection model from the tableview
     QItemSelectionModel *select = ui->status_tableView->selectionModel();
 
+    // Output the number of selected columns
     qDebug() << select->selectedColumns().length();
+
+    // Check to see if the number of selected columns is 1 (So we can graph it)
     if (select->selectedColumns().length() == 1)
     {
+        // Number of selected columns is 1
 
-        qDebug("We have one column");
-
+        // Get the index of the selected column
         int columnIndex = select->selectedIndexes().value(0).column();
 
+        // Output the number of indexes that we have selected (Should be number of rows)
         qDebug() <<  select->selectedIndexes().length();
+
+        // Iterate through the selected indexes and output to the debug log
+
+        // TODO #enhancement This data should be given to a graphing module which would
         for (int i = 0; i <= select->selectedIndexes().length(); i++)
         {
             qDebug() << i;
