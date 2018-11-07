b02a98a080801351a4b6102d2b870bac6a3ce71f
James Brown <james@impromptu-software.co.uk>
2018-11-07 14:22:31 +0000

    Add comments to topwindow.cpp

--- app/topwindow.cpp
+++ app/topwindow.cpp
@@ -6,19 +6,21 @@
 #include "gpswindow.h"
 
 
-topwindow::topwindow(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::topwindow)
+topwindow::topwindow(QWidget *parent):QWidget(parent),ui(new Ui::topwindow)
 {
+    // Called on init
+    // We should set up the ui for the top window:
     ui->setupUi(this);
 }
 
 topwindow::~topwindow()
 {
+    // Top window destructor
     delete ui;
 }
 void topwindow::openSecondWindow()
 {
+    // Open status data window
     showNewSecondWindow = new secondwindow();
 
     showNewSecondWindow -> show();
@@ -26,6 +28,7 @@ void topwindow::openSecondWindow()
 
 void topwindow::openGPSWindow()
 {
+    // Open GPS data window
     showNewGPSWindow = new gpswindow();
 
     showNewGPSWindow -> show();
@@ -33,6 +36,7 @@ void topwindow::openGPSWindow()
 
 void topwindow::openCurrentDataWindow()
 {
+    // Open Current Data window
     showNewCurrentStatusWindow = new currentstatus();
 
     showNewCurrentStatusWindow -> show();
@@ -40,15 +44,21 @@ void topwindow::openCurrentDataWindow()
 
 void topwindow::on_StatusButton_clicked()
 {
+    // Detected status button clicked
+    // We should try to open the second window
     openSecondWindow();
 }
 
 void topwindow::on_CurrentDataButton_clicked()
 {
+    // Detected current data button clicked
+    // We should try to open the current data window
     openCurrentDataWindow();
 }
 
 void topwindow::on_GPSButton_clicked()
 {
+    // Detected GPS button clicked
+    // We should try and open the current data window
     openGPSWindow();
 }
