41497c59ba5bd1cf3991d819610d134369d30622
charlie-wt <cwt1g14@soton.ac.uk>
2019-03-15 05:30:10 +0000

    removed spurious code.

--- utilities/packets/out_db_insert.py
+++ utilities/packets/out_db_insert.py
@@ -4,31 +4,6 @@ from util import field_size
 def out_db_insert (data, name):
     n = name[0].lower()
 
-    def field (name, val):
-        res = ''
-
-        f_name = c_name(name)
-        f_type = c_type(val['type'])
-        f_size = field_size(val)
-        f_len = val['length'] if 'length' in val else 1
-
-        if f_len == 1:
-            res = '\t{}.{} = static_cast<{}>(b.get({}));\n'.format(
-                n, f_name, f_type, f_size)
-        else:
-            actual_len = f_len + 1 if f_type == 'char' else f_len
-
-            res += '\t{} _{}[{}];\n'.format(f_type, f_name, actual_len)
-            res += '\tfor (int i=0; i<{}; i++) ' \
-                   '{{ _{}[i] = static_cast<{}>(b.get({})); }}\n'.format(
-                       f_len, f_name, f_type, f_size)
-            if f_type == 'char':
-                res += '\t_{}[{}] = \'\\0\';\n'.format(f_name, actual_len - 1)
-            res += '\tstd::copy(_{0}, _{0}+{1}, {2}.{0});\n'.format(
-                       f_name, actual_len, n)
-
-        return res
-
     out = 'bool DB::store_packet ({}& {}, Buffer b) {{\n'.format(name, n)
     out += '\tbool success = false;\n\n'
 
