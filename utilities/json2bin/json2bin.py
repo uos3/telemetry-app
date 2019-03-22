eb723543cef0053867122db1a4b30113e5753b50
corvidism <frankova@corvidism.com>
2019-03-22 12:16:04 +0100

    goals for the bin file generator tool

---
+++ utilities/json2bin/json2bin.py
@@ -0,0 +1,7 @@
+# input:
+#   json file with values
+#   yml file with fields
+#   something that describes the binfile structure (not sure if yml is enough?)
+
+# output:
+#   binary file compliant with the current packet spec (v0.2 at the moment)
