3db084dac9b4f6c540be7dd640318ae5e2000ccf
charlie-wt <cwt1g14@soton.ac.uk>
2019-06-23 12:51:05 +0100

    removed old structs folder.

--- utilities/packets/structs/img.h
+++
@@ -1,8 +0,0 @@
-struct Img {
-	char dataset_id[3];
-	uint32_t timestamp;
-	uint8_t image_id;
-	uint16_t fragment_id;
-	uint16_t num_fragments;
-	char image_data[6]; // TODO: replace once size is finalized
-};
--- utilities/packets/structs/packet.h
+++
@@ -1,8 +0,0 @@
-struct Packet {
-	char crc[3];
-	char hash[17];
-	PayloadType type;
-	Status status;
-	Payload payload;
-	uint32_t downlink_time;
-};
