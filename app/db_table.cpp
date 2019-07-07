a2e05d0dd75fed198a40dca345a002b51a5090bb
charlie-wt <cwt1g14@soton.ac.uk>
2019-07-07 18:22:56 +0100

    finished view of db tables.
    involved refactoring db.(h|cpp) to just be a free function
    (store_packet), since it wasn't really providing anything as a class
    anyway (and made some stuff here simpler).

---
+++ app/db_table.cpp
@@ -0,0 +1,41 @@
+#include "db_table.h"
+
+#include "QVBoxLayout"
+#include "QSizePolicy"
+
+
+DBTable::DBTable(
+	QSqlDatabase& db,
+	std::string table,
+	QWidget* parent,
+	Qt::WindowFlags f,
+	std::string sort_by)
+	: QWidget(parent, f)
+	, db(db)
+	, table(table)
+	, sort_by(sort_by)
+	, model(new QSqlQueryModel(this))
+	, view(new QTableView(this)) {
+
+	setLayout(new QVBoxLayout(this));
+	view->setModel(model);
+	layout()->addWidget(view);
+	layout()->setContentsMargins(5, 5, 5, 5);
+
+	model->setQuery(
+		QSqlQuery(QString::fromStdString(DBTable::query_string(table, sort_by)), db));
+	model->query().exec();
+}
+
+void DBTable::refresh (const Packet& p) {
+	Q_UNUSED(p);
+	model->setQuery(model->query());
+	model->query().exec();
+}
+
+std::string DBTable::query_string(std::string table, std::string sort_by) {
+	std::string q_str = "select * from " + table;
+	if (!sort_by.empty())
+		q_str += " order by " + sort_by + " desc";
+	return q_str + ";";
+}
---
+++ app/db_table.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "packet.h"
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QTableView>
+#include <QWidget>
+
+
+class DBTable : public QWidget {
+	Q_OBJECT
+
+public:
+	explicit DBTable (
+		QSqlDatabase& db,
+		std::string table,
+		QWidget* parent=nullptr,
+		Qt::WindowFlags f=Qt::WindowFlags(),
+		std::string sort_by="frame_id");
+
+public slots:
+	void refresh (const Packet& p);
+
+private:
+	static std::string query_string (std::string table, std::string sort_by);
+
+	QSqlDatabase& db;
+	std::string table;
+	std::string sort_by;
+
+	QSqlQueryModel* model;
+	QTableView* view;
+};
